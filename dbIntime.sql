520f1794bc6c1b7fec5c38d9cc99a478d6704ca4
PSigauque <psigauque.ps@gmail.com>
2019-09-28 19:09:47 +0200

    db

--- dbIntime.sql
+++ dbIntime.sql
@@ -1,78 +1,16 @@
-/*
-SQLyog Ultimate v10.00 Beta1
-MySQL - 5.5.5-10.4.6-MariaDB : Database - dbintime
-*********************************************************************
-*/
+DROP TABLE IF EXISTS `utilizador`;
 
-/*!40101 SET NAMES utf8 */;
-
-/*!40101 SET SQL_MODE=''*/;
-
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`dbintime` /*!40100 DEFAULT CHARACTER SET latin1 */;
-
-USE `dbintime`;
-
-/*Table structure for table `disciplina` */
-
-DROP TABLE IF EXISTS `disciplina`;
-
-CREATE TABLE `disciplina` (
-  `codigo` int(20) NOT NULL AUTO_INCREMENT,
-  `nomeDisciplina` varchar(255) NOT NULL,
-  `credito` int(20) DEFAULT NULL,
-  `cargaHoraria` int(255) DEFAULT NULL,
-  `regente` varchar(100) DEFAULT NULL,
-  `assistente` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`codigo`),
-  CONSTRAINT `docente` FOREIGN KEY (`codigo`) REFERENCES `docente` (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `disciplina` */
-
-/*Table structure for table `docente` */
-
-DROP TABLE IF EXISTS `docente`;
-
-CREATE TABLE `docente` (
-  `codigo` int(20) NOT NULL AUTO_INCREMENT,
-  `nomeDocente` varchar(255) NOT NULL,
-  `categoria` varchar(255) DEFAULT NULL,
+CREATE TABLE `utilizador` (
+  `codigo` INT(200) NOT NULL AUTO_INCREMENT,
+  `nomeUser` VARCHAR(200) DEFAULT NULL,
+  `entidade` VARCHAR(200) DEFAULT NULL,
+  `senha` VARCHAR(200) DEFAULT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=INNODB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
-/*Data for the table `docente` */
+/*Data for the table `utilizador` */
 
-/*Table structure for table `horario` */
-
-DROP TABLE IF EXISTS `horario`;
-
-CREATE TABLE `horario` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `semestre` varchar(2) NOT NULL,
-  `ano` int(4) DEFAULT NULL,
-  PRIMARY KEY (`codigo`),
-  CONSTRAINT `codigoTurma` FOREIGN KEY (`codigo`) REFERENCES `turma` (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `horario` */
-
-/*Table structure for table `turma` */
-
-DROP TABLE IF EXISTS `turma`;
-
-CREATE TABLE `turma` (
-  `codigo` int(11) NOT NULL AUTO_INCREMENT,
-  `nomeTurma` varchar(255) NOT NULL,
-  `nivel` int(3) DEFAULT NULL,
-  `tutor` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Data for the table `turma` */
+INSERT  INTO `utilizador`(`codigo`,`nomeUser`,`entidade`,`senha`) VALUES (1,'Ada','Administrador','ada111'),(2,'lua','Docente','lua111');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
