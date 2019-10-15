76560a8e8f2a6a477813371477259db3ea0bdff4
ima-mustafa <50020723+ima-mustafa@users.noreply.github.com>
2019-10-15 17:57:05 +0200

    updated db

--- dbintime.sql
+++ dbintime.sql
@@ -21,31 +21,31 @@ USE `dbintime`;
 DROP TABLE IF EXISTS `disciplina`;
 
 CREATE TABLE `disciplina` (
-  `codigo` int(255) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `credito` int(255) DEFAULT NULL,
-  `cargaHoraria` int(255) DEFAULT NULL,
-  `regente` varchar(255) DEFAULT NULL,
-  `assistente` varchar(255) DEFAULT NULL,
+  `codigo` INT(255) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `credito` INT(255) DEFAULT NULL,
+  `cargaHoraria` INT(255) DEFAULT NULL,
+  `regente` VARCHAR(255) DEFAULT NULL,
+  `assistente` VARCHAR(255) DEFAULT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=INNODB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 /*Data for the table `disciplina` */
 
-insert  into `disciplina`(`codigo`,`nome`,`credito`,`cargaHoraria`,`regente`,`assistente`) values (1,'EGO',5,90,'Emilio Mosse','Emilio Mosse'),(3,'FG',6,90,'Ida Alvarinho','Clarinda'),(4,'ADS I',4,120,'Cinquenta','Carlos Cumbana'),(5,'EDA',6,60,'Osvaldo Cossa','Osvaldo Cossa');
+INSERT  INTO `disciplina`(`codigo`,`nome`,`credito`,`cargaHoraria`,`regente`,`assistente`) VALUES (1,'EGO',5,90,'Emilio Mosse','Emilio Mosse'),(3,'FG',6,90,'Ida Alvarinho','Clarinda'),(4,'ADS I',4,120,'Cinquenta','Carlos Cumbana'),(5,'EDA',6,60,'Osvaldo Cossa','Osvaldo Cossa');
 
 /*Table structure for table `docente` */
 
 DROP TABLE IF EXISTS `docente`;
 
 CREATE TABLE `docente` (
-  `codigo` int(20) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(255) NOT NULL,
-  `categoria` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
-  `senha` varchar(255) DEFAULT NULL,
+  `codigo` INT(20) NOT NULL AUTO_INCREMENT,
+  `nome` VARCHAR(255) NOT NULL,
+  `categoria` VARCHAR(255) DEFAULT NULL,
+  `email` VARCHAR(255) DEFAULT NULL,
+  `senha` VARCHAR(255) DEFAULT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
 
 /*Data for the table `docente` */
 
@@ -54,17 +54,48 @@ CREATE TABLE `docente` (
 DROP TABLE IF EXISTS `horario`;
 
 CREATE TABLE `horario` (
-  `id` int(255) NOT NULL AUTO_INCREMENT,
-  `codigoTurma` int(255) NOT NULL,
-  `segunda` varchar(255) DEFAULT NULL,
-  `terca` varchar(255) DEFAULT NULL,
-  `quarta` varchar(255) DEFAULT NULL,
+  `id` INT(255) NOT NULL AUTO_INCREMENT,
+  `codigoTurma` INT(255) NOT NULL,
+  `segunda` VARCHAR(255) DEFAULT NULL,
+  `terca` VARCHAR(255) DEFAULT NULL,
+  `quarta` VARCHAR(255) DEFAULT NULL,
+  `quinta` VARCHAR(255) DEFAULT NULL,
+  `sexta` VARCHAR(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=INNODB DEFAULT CHARSET=latin1;
+
+/*Data for the table `horario` */
+
+/*Table structure for table `horariopresencas` */
+
+DROP TABLE IF EXISTS `horariopresencas`;
+
+CREATE TABLE `horariopresencas` (
+  `id` INT(255) NOT NULL,
+  `codigoTurma` VARCHAR(255) NOT NULL,
+  `segunda` VARCHAR(255) DEFAULT NULL,
+  `terca` VARCHAR(255) DEFAULT NULL,
+  `quarta` VARCHAR(255) DEFAULT NULL,
   `quinta` varchar(255) DEFAULT NULL,
   `sexta` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-/*Data for the table `horario` */
+/*Data for the table `horariopresencas` */
+
+/*Table structure for table `notificacoes` */
+
+DROP TABLE IF EXISTS `notificacoes`;
+
+CREATE TABLE `notificacoes` (
+  `codigo` int(255) NOT NULL,
+  `destinatario` varchar(255) DEFAULT NULL,
+  `assunto` varchar(500) DEFAULT NULL,
+  `corpo` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`codigo`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `notificacoes` */
 
 /*Table structure for table `turma` */
 
@@ -78,9 +109,11 @@ CREATE TABLE `turma` (
   `nivel` varchar(255) DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
 /*Data for the table `turma` */
+
+insert  into `turma`(`codigo`,`curso`,`tutor`,`regime`,`nivel`,`email`) values (1,'CIG','Paula','Laboral','I','Paula@gmail.com');
 
 /*Table structure for table `utilizador` */
 
